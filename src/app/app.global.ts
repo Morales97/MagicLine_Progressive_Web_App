c6b97ae68b493d181861932bc6824fb720c7d985
Daniel Morales Brotons <Dani@MacBook-MacBook-Pro-de-Daniel.local>
2020-01-13 12:18:42 +0100

    package dependencies fixed

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -4,6 +4,8 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class Globals {
     // readonly baseAppUrl: string = 'http://localhost:57431/';
+
     // readonly baseAPIUrl: string = 'https://192.168.1.34:3000';
+    //readonly baseAPIUrl: string = 'http://localhost:3000';
     readonly baseAPIUrl: string = 'https://localhost:3000';
 }
