520fc8c4a794bbe2006b6f5bfb787ea0b6afabc3
Daniel Morales Brotons <Dani@MacBook-MacBook-Pro-de-Daniel.local>
2020-02-02 21:08:20 +0100

    x

--- src/app/tram/tram.component.ts
+++ src/app/tram/tram.component.ts
@@ -14,6 +14,7 @@ export class TramComponent implements OnInit {
 
   currentUser: any;
   tram: any;
+  incidents: any[];
 
 
   constructor(
@@ -32,7 +33,9 @@ export class TramComponent implements OnInit {
   getOwnTram(){
     this.tramService.getOwnTram(this.global.baseAPIUrl + '/ownTram').subscribe(data =>{
       this.tram = data
-      console.log(data)
+      this.incidents = this.tram.incidents
+      console.log(this.tram)
+      console.log(this.incidents.length)
     })
   }
 
