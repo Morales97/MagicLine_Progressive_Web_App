6820e0cefa9d04d156d991aaac8c94cc5858db9d
Daniel Morales Brotons <Dani@MacBook-MacBook-Pro-de-Daniel.local>
2020-02-08 14:12:52 +0100

    url fix

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -8,7 +8,7 @@ export class Globals {
     // readonly baseAPIUrl: string = 'http://localhost:3000';
 
     // per utilitzar AWS
-    readonly baseAPIUrl: string = 'http://ec2-18-222-209-174.us-east-2.compute.amazonaws.com/';
+    readonly baseAPIUrl: string = 'http://ec2-18-222-209-174.us-east-2.compute.amazonaws.com/api';
 
     // local, HTTPS
     //readonly baseAPIUrl: string = 'https://localhost:3000';
