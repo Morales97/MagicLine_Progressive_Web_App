a5006a8b700ffdec6c46099cac76231dae64ac80
carlacampas <58336141+carlacampas@users.noreply.github.com>
2020-02-12 21:12:13 +0100

    responsive data tables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../_services';
+import { Globals } from '../app.global';
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"]
+  styleUrls: ["./home.component.scss"],
+  providers: [ Globals ]
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
-  test_var: any;
+  usersList: any;
+  tramsList: any;
 
-  constructor(
-    ) {}
+  constructor(private adminService: AdminService, private global : Globals) { }
 
+  ngOnInit() {
+    this.getUsersList();
+    this.getTramsList();
+  }
+
+  getUsersList() {
+    this.adminService.getUsers(this.global.baseAPIUrl + '/users').subscribe(data => {
+      this.usersList = data
+    })
+  }
+
+  getTramsList() {
+    this.adminService.getTrams(this.global.baseAPIUrl + '/trams').subscribe(data => {
+      this.tramsList = data
+    })
+  }
 }
